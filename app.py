ca340eb2695d822446f82019945ed7fb3e974f92
YanaPalyvoda <66125524+YanaPalyvoda@users.noreply.github.com>
2021-07-24 21:14:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ This sample shows how to create a bot that demonstrates the following:
 
 from http import HTTPStatus
 
-from aiohttp import web
-from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
     BotFrameworkAdapterSettings,
     ConversationState,
@@ -33,6 +31,10 @@ from bots import DialogAndWelcomeBot
 
 from adapter_with_error_handler import AdapterWithErrorHandler
 from flight_booking_recognizer import FlightBookingRecognizer
+
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
+
 CONFIG = DefaultConfig()
 
 # Create adapter.
